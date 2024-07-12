27eb37e283f499b457949a696c0833461cc52242
Arthur <0xa1337@proton.me>
2024-07-13 01:35:26 +0200

    feat: define shared message library

---
+++ contracts/librairies/MessageCodecs.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+library MessageCodecs {
+    uint8 internal constant MSG_BUY_REMOTE = 0;
+    uint8 internal constant MSG_SELL_REMOTE = 1;
+}
