df5eb23fc5d05b08933bc949e25585b7a1933252
Arthur <0xa1337@proton.me>
2024-07-13 15:04:26 +0200

    feat: setup support for contract verification

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,10 +5,11 @@
 // - Fill in the environment variables
 import 'dotenv/config'
 
-import 'hardhat-deploy'
-import 'hardhat-contract-sizer'
-import '@nomiclabs/hardhat-ethers'
 import '@layerzerolabs/toolbox-hardhat'
+import '@nomicfoundation/hardhat-verify'
+import '@nomiclabs/hardhat-ethers'
+import 'hardhat-contract-sizer'
+import 'hardhat-deploy'
 import { HardhatUserConfig, HttpNetworkAccountsUserConfig } from 'hardhat/types'
 
 import { EndpointId } from '@layerzerolabs/lz-definitions'
@@ -21,6 +22,8 @@ const MNEMONIC = process.env.MNEMONIC
 
 // If you prefer to be authenticated using a private key, set a PRIVATE_KEY environment variable
 const PRIVATE_KEY = process.env.PRIVATE_KEY
+
+const SCROLL_API_KEY = process.env.SCROLL_API_KEY
 
 const accounts: HttpNetworkAccountsUserConfig | undefined = MNEMONIC
     ? { mnemonic: MNEMONIC }
@@ -54,24 +57,69 @@ const config: HardhatUserConfig = {
     networks: {
         baseSepolia: {
             eid: EndpointId.BASESEP_V2_TESTNET,
-            url: "https://sepolia.base.org",
+            url: 'https://sepolia.base.org',
             accounts,
         },
         scrollSepolia: {
             eid: EndpointId.SCROLL_V2_TESTNET,
-            url: "https://sepolia-rpc.scroll.io",
+            url: 'https://sepolia-rpc.scroll.io',
             accounts,
         },
         arbitrumSepolia: {
             eid: EndpointId.ARBSEP_V2_TESTNET,
-            url: "https://sepolia-rollup.arbitrum.io/rpc",
+            url: 'https://sepolia-rollup.arbitrum.io/rpc',
             accounts,
         },
         optimismSepolia: {
             eid: EndpointId.OPTSEP_V2_TESTNET,
-            url: "https://sepolia.optimism.io",
+            url: 'https://sepolia.optimism.io',
             accounts,
         },
+    },
+    etherscan: {
+        apiKey: {
+            baseSepolia: 'mock', // not required by blockscout
+            arbitrumSepolia: 'mock', // not required by blockscout
+            optimismSepolia: 'mock', // not required by blockscout
+            scrollSepolia: SCROLL_API_KEY || '',
+        },
+        customChains: [
+            {
+                network: 'baseSepolia',
+                chainId: 84532,
+                urls: {
+                    apiURL: 'https://base-sepolia.blockscout.com/api',
+                    browserURL: 'https://base-sepolia.blockscout.com/',
+                },
+            },
+            {
+                network: 'arbitrumSepolia',
+                chainId: 421614,
+                urls: {
+                    apiURL: 'https://arbitrum-sepolia.blockscout.com/api',
+                    browserURL: 'https://arbitrum-sepolia.blockscout.com/',
+                },
+            },
+            {
+                network: 'optimismSepolia',
+                chainId: 11155420,
+                urls: {
+                    apiURL: 'https://optimism-sepolia.blockscout.com/api',
+                    browserURL: 'https://optimism-sepolia.blockscout.com/',
+                },
+            },
+            {
+                network: 'scrollSepolia',
+                chainId: 534351,
+                urls: {
+                    apiURL: 'https://api-sepolia.scrollscan.com/api',
+                    browserURL: 'https://sepolia.scrollscan.com/',
+                },
+            },
+        ],
+    },
+    sourcify: {
+        enabled: false,
     },
     namedAccounts: {
         deployer: {
