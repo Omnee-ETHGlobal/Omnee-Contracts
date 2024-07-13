d6b7118b66c08c37ed09dda513fb00f2c3f6e850
Arthur <0xa1337@proton.me>
2024-07-14 00:53:23 +0200

    feat: make bondingcurve crosschain

--- contracts/BondingCurve.sol
+++ contracts/BondingCurve.sol
@@ -1,16 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { IOFT, SendParam } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/interfaces/IOFT.sol";
+import { OApp, MessagingFee } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/OApp.sol";
+import { ILayerZeroComposer } from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/ILayerZeroComposer.sol";
+import { OptionsBuilder } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/libs/OptionsBuilder.sol";
 
 import "./librairies/MsgUtils.sol";
 
-contract BondingCurve is ReentrancyGuard, Ownable {
+contract BondingCurve is OApp, ILayerZeroComposer, ReentrancyGuard, Ownable {
     using SafeERC20 for IERC20;
+    using OptionsBuilder for bytes;
 
     struct TokenInfo {
         uint256 reserveBalance;
@@ -79,6 +84,64 @@ contract BondingCurve is ReentrancyGuard, Ownable {
         emit TokenBought(msg.sender, _tokenAddress, buyableAmount);
     }
 
+    function _buyTokensRemote(address _tokenAddress, address _buyer, uint256 _ethAmount, uint32 _eid) private {
+        TokenInfo storage tokenInfo = supportedTokens[_tokenAddress];
+        if (!tokenInfo.exists) revert TokenNotSupported(_tokenAddress);
+
+        IOFT oft = IOFT(_tokenAddress);
+        SendParam memory quoteSendParam = SendParam({
+            dstEid: _eid,
+            to: MsgUtils.addressToBytes32(_buyer),
+            amountLD: 0, // We don't know yet
+            minAmountLD: 0, // We don't know yet
+            extraOptions: bytes(""),
+            composeMsg: bytes(""),
+            oftCmd: bytes("")
+        });
+
+        // Estimate fees to send OFTs back to the buyer
+        MessagingFee memory fee = oft.quoteSend(quoteSendParam, false);
+        require(_ethAmount >= fee.nativeFee, "Insufficient fee provided");
+
+        // Calculate the amount of tokens the buyer can buy after fees
+        uint256 finalEthAmount = _ethAmount - fee.nativeFee;
+
+        // The amount of tokens the buyer can buy
+        uint256 buyableAmount = calculateBuyableAmount(_tokenAddress, finalEthAmount);
+        if (tokenInfo.reserveBalance < buyableAmount) revert InsufficientBalance(_tokenAddress);
+
+        tokenInfo.reserveBalance -= buyableAmount;
+        tokenInfo.liquidity += finalEthAmount;
+
+        quoteSendParam.amountLD = buyableAmount;
+        quoteSendParam.minAmountLD = buyableAmount;
+
+        IOFT(_tokenAddress).send(quoteSendParam, MessagingFee(fee.nativeFee, 0), payable(this)); // ? what's the best thing to do here
+
+        emit TokenBought(_buyer, _tokenAddress, buyableAmount);
+    }
+
+    function _sellTokensRemote(address _tokenAddress, address _buyer, uint256 _tokenAmount, uint32 _eid) private {
+        if (_tokenAmount == 0) revert InvalidAmount(_tokenAmount);
+        TokenInfo storage tokenInfo = supportedTokens[_tokenAddress];
+        if (!tokenInfo.exists) revert TokenNotSupported(_tokenAddress);
+
+        IOFT oft = IOFT(_tokenAddress);
+
+        bytes memory extraOptions = OptionsBuilder.newOptions().addExecutorNativeDropOption(payout, _buyer);
+        MessagingFee memory fee = _quote(_eid, bytes(""), extraOptions, false);
+        require(payout >= fee.nativeFee, "Payout is insufficient to cover fees");
+        uint256 payout = calculateSellPayout(_tokenAddress, _tokenAmount);
+        require(address(this).balance >= payout, "Insufficient contract ETH balance");
+
+        tokenInfo.reserveBalance += _tokenAmount;
+        tokenInfo.liquidity -= payout;
+
+        _lzSend(_eid, bytes(""), extraOptions, MessagingFee(payout, 0), payable(this)); // TODO: rf user? contract?
+
+        emit TokenSold(_buyer, _tokenAddress, finalTokenAmount, payout);
+    }
+
     function sellTokens(address _tokenAddress, uint256 _amount) external nonReentrant {
         if (_amount == 0) revert InvalidAmount(_amount);
         TokenInfo storage tokenInfo = supportedTokens[_tokenAddress];
@@ -108,13 +171,44 @@ contract BondingCurve is ReentrancyGuard, Ownable {
     function calculateSellPayout(address _tokenAddress, uint256 _tokenAmount) public pure returns (uint256) {
         return _tokenAmount * INITIAL_PRICE; // TODO: Make this variable
     }
-    
-    
+
     function getTokenPrice(address _tokenAddress) public view returns (uint256) {
         TokenInfo memory tokenInfo = supportedTokens[_tokenAddress];
         if (!tokenInfo.exists) revert TokenNotSupported(_tokenAddress);
 
         return INITIAL_PRICE; // TODO: Make this variable
+    }
+
+    function lzReceive(
+        Origin calldata _origin,
+        bytes32 _guid,
+        bytes calldata _message,
+        address _executor,
+        bytes calldata _extraData
+    ) external payable override {
+        (uint8 msgType, address tokenAddress, address sender, uint32 eid, ) = abi.decode(
+            _message,
+            (uint8, address, address, uint32, uint256)
+        );
+
+        if (msgType == 1) {
+            _buyTokensRemote(tokenAddress, sender, msg.value, eid);
+        } else {
+            revert InvalidMessageType(msgType);
+        }
+    }
+
+    function lzCompose(address, bytes32, bytes calldata _message, address, bytes calldata) external payable override {
+        (uint8 msgType, address tokenAddress, address sender, uint32 eid, uint256 tokenAmount) = abi.decode(
+            _message,
+            (uint8, address, address, uint32, uint256)
+        );
+
+        if (msgType == 2) {
+            _sellTokensRemote(tokenAddress, sender, tokenAmount, eid);
+        } else {
+            revert InvalidMessageType(msgType);
+        }
     }
 
     fallback() external payable {}
