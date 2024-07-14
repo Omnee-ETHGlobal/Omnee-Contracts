790eb8e14007a658ae238045aa1a73fdde8d1fdf
Arthur <0xa1337@proton.me>
2024-07-14 06:26:58 +0200

    chore: comment oft factory

--- contracts/OFTFactory.sol
+++ contracts/OFTFactory.sol
@@ -6,19 +6,28 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { Create3 } from "./librairies/Create3.sol";
 import { OmneeOFT } from "./OmneeOFT.sol";
 
+/// @title OFTFactory
+/// @notice Factory contract for deploying OmneeOFT tokens across different chains
+/// @dev Uses LayerZero for cross-chain communication and Create3 for deterministic deployments
 contract OFTFactory is OAppReceiver {
+    // State variables
     uint32 public eid;
     bytes32 public universalFactory;
     string public salt = "OMNEE_OFT";
-
     uint32 public baseEID = 40245;
+    address public bondingCurve; /// Address passed to the OFT for initial mint
 
+    // Mappings
     mapping(uint256 => address) public deployIdToAddress;
 
-    address public bondingCurve; /// Passed to the OFT for initial mint
-
+    // Events
     event OFTCreated(address indexed sender, string indexed name, string symbol, uint32 eid, uint256 deployId);
 
+    /// @notice Constructor to initialize the OFTFactory
+    /// @param _endpoint Address of the LayerZero endpoint
+    /// @param _owner Address of the contract owner
+    /// @param _eid Endpoint ID for this chain
+    /// @param _universalFactory Address of the universal factory
     constructor(
         address _endpoint,
         address _owner,
@@ -29,12 +38,16 @@ contract OFTFactory is OAppReceiver {
         universalFactory = _universalFactory;
     }
 
+    /// @notice Handles incoming LayerZero messages to deploy OFTs
+    /// @dev Only allows messages from the universal factory
+    /// @param _origin Origin information of the LayerZero message
+    /// @param payload Encoded deployment information
     function _lzReceive(
         Origin calldata _origin,
         bytes32,
         bytes calldata payload,
         address,
-        bytes calldata // Any extra data or options to trigger on receipt.
+        bytes calldata
     ) internal override {
         require(_origin.sender == universalFactory, "Unauthorized");
 
@@ -56,6 +69,13 @@ contract OFTFactory is OAppReceiver {
         emit OFTCreated(oftAddr, _name, _symbol, _eid, _deployId);
     }
 
+    /// @notice Deploys an OFT on the Base chain
+    /// @dev Can only be called by the universal factory
+    /// @param _name Name of the OFT
+    /// @param _symbol Symbol of the OFT
+    /// @param _deployId Unique deployment ID
+    /// @param admin Address of the OFT admin
+    /// @return Address of the deployed OFT
     function deployOFTBase(
         string memory _name,
         string memory _symbol,
@@ -77,6 +97,9 @@ contract OFTFactory is OAppReceiver {
         return oftAddr;
     }
 
+    /// @notice Sets the address of the bonding curve contract
+    /// @dev Can only be called by the contract owner
+    /// @param _bondingCurve Address of the new bonding curve contract
     function setBondingCurve(address _bondingCurve) public onlyOwner {
         bondingCurve = _bondingCurve;
     }
